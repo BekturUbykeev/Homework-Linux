bdb1114145b5397d43f304c9c96e540984fb91c6
EC2 Default User <ec2-user@ip-172-31-89-124.ec2.internal>
2024-12-06 20:23:55 +0000

    age calculator

---
+++ homework9/age.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+read -p "Enter your birth year: " year
+
+current_year=2024
+
+echo you are $((current_year-year)) years old
+
+echo in 2020 you were $((2020-year)) years old
+
+echo in 2030 you will be $((2030-year)) years old
+
+
